73537f93341da84df1dde88fb7842aae645f4137
Rishu Sahu <rishu28112003@gmail.com>
2025-01-18 21:08:25 +0530

    Initial commit

---
+++ 01 introduction/Program/02_variables_Comments.cpp
@@ -0,0 +1,14 @@
+using namespace std;
+#include<iostream>
+
+// This is ceated by Rishu
+/* This is 
+ multi lelvel
+  Comments*/
+
+  main()
+  {
+    int sum = 10;
+    cout<<"Rishu"<<sum;
+    return 0;
+  }
